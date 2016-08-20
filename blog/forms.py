3ec1e1ced8ba27ead50b932ca363e2261b377fc1
twoutlook <twcloudwebapp@outlook.com>
2016-08-20 11:13:53 +0000

    better layout for list,detail and edit, BUT EDIT..

--- blog/forms.py
+++ blog/forms.py
@@ -9,7 +9,9 @@ class PostForm(forms.ModelForm):
         fields = ('title', 
         # 'brief',
         # 'text',
+        'r01c4','r01c6','r01c8',
         'r03c3','r03c4','r03c5','r03c6','r03c7',
+        'r04c3','r04c4','r04c5','r04c6','r04c7',
         )
         
         #   <tr>
--- blog/models.py
+++ blog/models.py
@@ -12,12 +12,25 @@ class Post(models.Model):
     published_date = models.DateTimeField(
             blank=True, null=True)
 
+   
+    r01c4=models.CharField(default='', max_length=99)
+    r01c6=models.CharField(default='', max_length=99)
+    r01c8=models.CharField(default='', max_length=99)
+   
+   
     #
     r03c3=models.CharField(default='', max_length=99)
     r03c4=models.CharField(default='', max_length=99)
     r03c5=models.CharField(default='', max_length=99)
     r03c6=models.CharField(default='', max_length=99)
     r03c7=models.CharField(default='', max_length=99)
+
+    r04c3=models.CharField(default='', max_length=99)
+    r04c4=models.CharField(default='', max_length=99)
+    r04c5=models.CharField(default='', max_length=99)
+    r04c6=models.CharField(default='', max_length=99)
+    r04c7=models.CharField(default='', max_length=99)
+
           
     def publish(self):
         self.published_date = timezone.now()
